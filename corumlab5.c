d9a3a91f459a1c95f427d2f01d61c8e8e5a0122e
Cloud User <ec2-user@ip-172-31-14-89.us-east-2.compute.internal>
2017-03-02 13:03:28 -0500

    Just Started

---
+++ corumlab5.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+int choice=0;
+printf("#Menu \n1.Enter Attendee Information \n2. List All Attendees \n3. Quit\n\nWhat do you want to do? (1/2/3): ");
+while(choice == 0){
+scanf(" %d", &choice);
+}
+
+return 0;
+}
+
