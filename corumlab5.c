238e9db53e3f310ac904be1c1841eba57f96aa50
Cloud User <ec2-user@ip-172-31-14-89.us-east-2.compute.internal>
2017-03-12 14:52:54 -0400

    Starting on getting attendees

--- corumlab5.c
+++ corumlab5.c
@@ -2,12 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main() {
 int choice=0;
+char firstName[31], middleName[31], lastName[31], association[31], title[31], input[128];
+int month, day, year;
+
 printf("#Menu \n1.Enter Attendee Information \n2. List All Attendees \n3. Quit\n\nWhat do you want to do? (1/2/3): ");
 while(choice == 0){
 scanf(" %d", &choice);
+}
+
+while(choice == 1){
+title[0]=' ';
+printf("\n#Enter Attendee Information\n");
+printf("Title: ");
+while(title[0]==' '){
+fgets(input, 128, stdin);
+sscanf(input, "%31s", title);
+}
+if(title[0] == '\n'){break;}
+printf("First Name: ");
+fgets(input, 128, stdin);
+sscanf(input, "%31s", firstName);
+
+printf("Middle Name: ");
+fgets(input, 128, stdin);
+sscanf(input, "%31s", firstName);
+
+printf("Last Name: ");
+fgets(input, 128, stdin);
+sscanf(input, "%31s", lastName);
+
+printf("Association: ");
+fgets(input, 128, stdin);
+sscanf(input, "%31s", association);
 }
 
 return 0;
