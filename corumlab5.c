c0c73c95cbfb287038e852d1b5dfd8e969d406c4
Cloud User <ec2-user@ip-172-31-14-89.us-east-2.compute.internal>
2017-03-18 23:39:22 -0400

    push method does not work at all

--- corumlab5.c
+++ corumlab5.c
@@ -4,16 +4,20 @@
 #include <time.h>
 #include <string.h>
 
-struct addendee{
+struct attendee{
 int id;
 char firstName[31], middleName[31], lastName[31], association[31], title[31];
-struct addendee *next;
+struct attendee *prioradd;
 };
+
+struct attendee *tosp;
+void push(struct attendee *);
 
 int main() {
 int choice=0;
 char firstName[31], middleName[31], lastName[31], association[31], title[31], input[128];
 int month, day, year;
+struct attendee tmp;
 
 printf("#Menu \n1.Enter Attendee Information \n2. List All Attendees \n3. Quit\n\nWhat do you want to do? (1/2/3): ");
 while(choice == 0){
@@ -21,32 +25,53 @@ scanf(" %d", &choice);
 }
 
 while(choice == 1){
-title[0]=' ';
+tmp.title[0] = ' ';
 printf("\n#Enter Attendee Information\n");
 printf("Title: ");
-while(title[0]==' '){
+while(tmp.title[0]==' '){
 fgets(input, 128, stdin);
-sscanf(input, "%31s", title);
+sscanf(input, "%31s", tmp.title);
 }
-if(title[0] == '\n'){break;}
+/*if(tmp.title[0] == '\n'){choice  = 0; break;}*/
+
 printf("First Name: ");
 fgets(input, 128, stdin);
-sscanf(input, "%31s", firstName);
+sscanf(input, "%31s", tmp.firstName);
 
 printf("Middle Name: ");
 fgets(input, 128, stdin);
-sscanf(input, "%31s", firstName);
+sscanf(input, "%31s", tmp.middleName);
 
 printf("Last Name: ");
 fgets(input, 128, stdin);
-sscanf(input, "%31s", lastName);
+sscanf(input, "%31s", tmp.lastName);
 
 printf("Association: ");
 fgets(input, 128, stdin);
-sscanf(input, "%31s", association);
+sscanf(input, "%31s", tmp.association);
 
+tosp = &tmp;
+push(tosp);
 }
 
 return 0;
 }
 
+void push(struct attendee *attendee){
+struct attendee *new;
+
+new = (struct attendee *) malloc(sizeof(struct attendee));
+if(new == (struct attendee *) NULL){
+printf("\nFailed to allocate mem\n");
+exit(1);
+}
+/*
+new.title = attendee->title;
+new.firstName = attendee.firstName;
+new.middleName = attendee.middleName;
+new.lastName = attendee.lastName;
+new.association = attendee.association;
+
+new.prioradd = tosp;
+tosp = new;*/
+}
